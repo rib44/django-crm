037d1f3a8eae34bc3afa0974d8761a0c46a39349
Aryan Kumar <aryankr730@gmail.com>
2023-04-22 17:09:19 +0530

    Added the registration functionality

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -37,4 +38,23 @@ def logout_user(request):
     return render(request, 'logout.html', {})
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered")
+
+            return redirect('home')
+
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
